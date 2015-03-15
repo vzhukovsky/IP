a76ef00b156147335e55a01da6e34008c730a3df
Valentin <kventin_zhuk@outlook.com>
2015-03-14 18:25:33 -0700

    initial setup

---
+++ ImagesProcessing/ImagesProcessing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ImagesProcessing
+{
+    public class Objfdf
+    {
+        public int Method(int a)
+        {
+            return 0;
+        }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var a = 0;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
